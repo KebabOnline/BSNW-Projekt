bba7b0e9ad94b73b107763015149541c8192fe60
KebabOnline <merek77777@gmail.com>
2024-11-06 01:23:16 +0100

    echo server fix?

--- src/tcp/EchoServer.java
+++ src/tcp/EchoServer.java
@@ -7,8 +7,7 @@ import java.io.OutputStream;
 public class EchoServer implements ConnectionHandler{
     @Override
     public void handleConnection(InputStream is, OutputStream os) throws IOException {
-        int receivedByte = is.read(); // Read Byte
-        os.write(receivedByte); // Write same Byte
+        os.write(is.read()); // Write same Byte that is read
 
         is.close(); // TCP signalisiert dem anderen Prozess, dass nichts mehr kommt.
         os.close(); // dem OS wird gesagt, dass man nichts mehr empfangen will.
